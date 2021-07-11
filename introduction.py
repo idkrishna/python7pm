f773606c12231487774b3682e80c82d40b842d8d
idkrishna <idkrishna@gmail.com>
2021-07-11 21:00:24 +0545

    hello

---
+++ introduction.py
@@ -0,0 +1,35 @@
+fName = "Krishna"
+lName = "Giri"
+age = 35
+# print("{} {} {}" .format(fName, lName, age))
+# print(f"{fName} {lName} {age}")
+myName = "Ram"
+age = 20
+# print(f"Name:{myName} Age: {age}")
+# name = input("Enter any name: ")
+# print(f"You enter {name}")
+# x=input("Enter x: ")
+# print(type(x))
+# y = input ("Enter y: ")
+# x=int(x)
+# y=int(y)
+
+# print (x+y)
+# x = int (input("Enter x: "))
+# y = int(input("Enter y: "))
+# print(x+y)
+
+# x = input("Enter x: ")
+# y = input("Enter y: ")
+# result = int(x) + int(y)
+# print(result)
+
+# name, age, phone
+# ipaddress
+
+name = input("Enter name: ")
+age = input("Enter age: ")
+phone = input("Enter phone: ")
+address = input("Enter address: ")
+print(f"Name: {name} Age: {age} Phone: {phone} Address: {address}")
+
